29af00c5ddd8385ef307e575c1450bb22ee8cd4c
Arnold Camas <arnoldc3@hexaware.com>
2022-01-20 21:51:23 +0800

    progress 1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <template>
   <div class="app">
-    hello, hyrule
+   <JobList :jobs="jobs" />
   </div>
+  <button @click="changeName('hola')">Change message</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs, ref } from 'vue';
+import Job from './types/Job';
+import JobList from './components/JobsList.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "ranch",
+        salary: 3000,
+        id: "1"
+      },
+      {
+        title: "dish worker",
+        location: "restaraunt",
+        salary: 3000,
+        id: "2"
+      },
+    ]);
+
+    return {
+      jobs,
+    }
+
+
+    // const state = reactive({
+    //   message: 'Hello Vue!',
+    // });
+
+
+    // return {
+    //   ...toRefs(state)
+    // }'
+
+    // const name = ref('Vue');
+    // const age = ref<string|number>(18);
+
+    // return {
+    //   name,
+    //   age,
+    // } 
+  },
 });
 </script>
 
---
+++ src/components/JobsList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="job-list">
+        <ul>
+            <li v-for="job in jobs" :key="job.id">
+                <h2>{{ job.title }}</h2>
+                <p> {{ job.location }} </p>
+            </li>   
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Job from '@/types/Job';
+
+export default defineComponent({
+    props: {
+        jobs: {
+            type: Array as PropType<Job[]>,
+            required: true,
+        },
+    },
+})
+</script>
+
+
+<style scoped>
+
+</style>
